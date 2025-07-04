525601457e98caec37577fd9d63d3dbb9f9f0377
Vellankijay <93359044+Vellankijay@users.noreply.github.com>
2025-07-04 11:35:05 -0500

    done with changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -96,20 +96,32 @@ const Header = () => {
                     Home
                     <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-600 group-hover:w-full transition-all duration-300"></span>
                 </a>
-                <a className = "relative text-gray-600 dark: text-gray-200 hover:orange-600 dark:hover:text-orange-400 font-medium transition-colors duration-300 group z-50" href="#">
+                <a onClick={toggleMobileMenu}
+                className = "relative text-gray-600 dark: text-gray-200 hover:orange-600 dark:hover:text-orange-400 font-medium transition-colors duration-300 group z-50" href="#aboutMe">
                     About Me
                     <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-600 group-hover:w-full transition-all duration-300"></span>
                 </a>
-                <a className = "relative text-gray-600 dark: text-gray-200 hover:orange-600 dark:hover:text-orange-400 font-medium transition-colors duration-300 group z-50" href="#">
+                <a 
+                onClick={toggleMobileMenu}
+                className = "relative text-gray-600 dark: text-gray-200 hover:orange-600 dark:hover:text-orange-400 font-medium transition-colors duration-300 group z-50" href="#experience">
                     Experience
                     <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-600 group-hover:w-full transition-all duration-300"></span>
                 </a>
-                <a className = "relative text-gray-600 dark: text-gray-200 hover:orange-600 dark:hover:text-orange-400 font-medium transition-colors duration-300 group z-50" href="#">
+                <a 
+                onClick={toggleMobileMenu}
+                className = "relative text-gray-600 dark: text-gray-200 hover:orange-600 dark:hover:text-orange-400 font-medium transition-colors duration-300 group z-50" href="#projects">
                     Projects
                     <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-600 group-hover:w-full transition-all duration-300"></span>
                 </a>
-                <button className="hidden md:block bg-[#a7a7a7] text-black py-3 px-8 rounded-full border-none font-medium
-                    transition-all duration-500 hover:bg-white cursor-pointer z-50">
+                <button className="md:block bg-[#a7a7a7] text-black py-3 px-8 rounded-full border-none font-medium
+                    transition-all duration-500 hover:bg-white cursor-pointer z-50"
+                    onClick={() => {
+                        const section = document.getElementById("contactMe");
+                        if (section) {
+                          section.scrollIntoView({ behavior: "smooth" });
+                        }
+                        toggleMobileMenu();
+                      }}>
                     Contact Me
                 </button>
             </nav>
